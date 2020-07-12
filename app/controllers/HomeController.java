789a40e3f65645016c4fa0ba9289fbc069ebb3a5
WKB-I <0424wakaba@gmail.com>
2020-07-12 23:16:41 +0900

    Cookie processing. It's working somehow.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -18,9 +18,21 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index(int id, Optional name) {
-        return ok("<title>Hello!</title><h1>Hello!</h1><p>ID ="+ id + ", NAME = "+ name.orElse("no-name") + "です。</p>")
-                .as("text/html");
+    public Result index(Optional<String> name) {
+        String param = name.orElse("");
+        String message = "<p>nameはありません。</p>";
+        if(param != ""){
+            message = "<p>nameが送られました。</p>";
+            Http.Cookie newcookie = Http.Cookie.builder("name", param).build();
+            response().setCookie(newcookie);
+        }
+        Http.Cookie cookie = request().cookie("name");
+        if(cookie == null){
+            message += "<p>cookie: no-cookie.</p>";
+        }else{
+            message += "<p>cookie: " + cookie.value() + "</p>";
+        }
+        return ok("<title>Hello!</title><h1>Hello!</h1>" + message).as("text/html");
     }
 }
 
