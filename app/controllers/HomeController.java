d3d1d15efb25fbb4e0a4d5ed8da12ba0ac11c4fa
WKB-I <0424wakaba@gmail.com>
2020-07-30 02:02:55 +0900

    Add the create process

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -30,5 +30,36 @@ public class HomeController extends Controller {
         }catch (SQLException e){}
         return ok(views.html.index.render(msg));
     }
+
+    private final Form<PersonForm> personform;
+
+    @Inject
+    public HomeController(FormFactory factory){
+        this.personform = factory.form(PersonForm.class);
+    }
+
+    public Result add(){
+        return ok(views.html.add.render("Please fill out the form", personform));
+    }
+
+    public Result create(){
+        Form<PersonForm> formdata = personform.bindFromRequest();
+        PersonForm form = formdata.get();
+        String name = form.getName();
+        String mail = form.getMail();
+        String telephonenumber = form.getTelephoneNumber();
+        try{
+            Connection connection = db.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into people values(default, ?, ?, ?)");
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, mail);
+            preparedStatement.setString(3, telephonenumber);
+            preparedStatement.executeUpdate();
+        }catch (SQLException e){
+            return ok(views.html.add.render("Please fill out the form", formdata));
+        }
+        return redirect(routes.HomeController.index());
+    }
+
 }
 
---
+++ app/controllers/PersonForm.java
@@ -0,0 +1,47 @@
+package controllers;
+
+public class PersonForm {
+    protected int id;
+    protected String name;
+    protected String mail;
+    protected String telephoneNumber;
+
+    public PersonForm(){
+        super();
+    }
+
+
+
+    public PersonForm(int id){
+        super();
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+}
