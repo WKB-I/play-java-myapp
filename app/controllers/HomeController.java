fd129a0163ad84c398983485f17abb61e4f30d92
WKB-I <0424wakaba@gmail.com>
2020-07-29 23:16:37 +0900

    Database Access by JDBC

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,6 +4,8 @@ import play.mvc.*;
 import java.util.*;
 import javax.inject.*;
 import play.data.*;
+import play.db.*;
+import java.sql.*;
 
 import static play.libs.Scala.asScala;
 
@@ -12,30 +14,21 @@ import static play.libs.Scala.asScala;
  * to the application's home page.
  */
 public class HomeController extends Controller {
-    private final Form<MyForm> myform;
-
-    @Inject
-    public HomeController(FormFactory factory){
-        this.myform = factory.form(MyForm.class);
-    }
+    @Inject Database db;
 
     public Result index() {
-        return ok(views.html.index.render(
-                "This is a message I prepared for you at the controller.",
-                this.myform
-        ));
-    }
-
-    public Result form(){
-        Form<MyForm> form = this.myform.bindFromRequest();
-        MyForm myform = form.get();
-        String name = myform.getName();
-        String password = myform.getPassword();
-        String radio = myform.getRadio();
-        return ok(views.html.index.render(
-           "This message is after send form.\n" + "name:" + name + ", password:" + password + ", radio:" + radio,
-            form
-        ));
+        String msg = "database record:<br>";
+        try{
+            Connection connection = db.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * from people");
+            msg += "<ul>";
+            while (resultSet.next()){
+                msg += "<li>" + resultSet.getInt("id") + ":" + resultSet.getString("name") + "</li>";
+            }
+            msg += "</ul>";
+        }catch (SQLException e){}
+        return ok(views.html.index.render(msg));
     }
 }
 
--- build.sbt
+++ build.sbt
@@ -8,3 +8,6 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.13.2"
 
 libraryDependencies += guice
+libraryDependencies += "com.h2database" % "h2" % "1.4.197"
+libraryDependencies += evolutions
+libraryDependencies += javaJdbc
